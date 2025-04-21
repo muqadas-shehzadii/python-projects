4330cbdf2728d186b5ab6bc59d0fa90a50e9e7db
Muqadas Shehzadi <muqadasshahzedi@gmail.com>
2025-04-21 09:27:10 -0700

    done
    by using selection structure!

---
+++ age_group_categorizatin.py
@@ -0,0 +1,12 @@
+#Question: classify a peson's group age: Child(<13), Teenager(13-19),Adult(20,59),Senior(60+) 
+
+
+age = 25
+if age<13:
+	print("child")
+elif age<20:
+	print("Teenager")
+elif age<60:
+	print("Adult")
+else:
+	print("Senior")
